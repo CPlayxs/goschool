456be3302f1b6a9dae27712a909ea3ecff825f21
Wincohax <wincohax@proton.me>
2024-06-05 16:24:43 -0500

    Modifing Scenes and adding new Stuff

--- scripts/PlayB1.gd
+++ scripts/PlayB1.gd
@@ -2,4 +2,4 @@ extends Button
 
 func _on_pressed():
 	pass # Replace with function body.
-	get_tree().change_scene_to_file("res://scenes/node_3d.tscn")
+	get_tree().change_scene_to_file("res://scenes/room1.tscn")
